8baa8f74f23dd327608497beca243329744bcbd6
Jacob Brown <j.h.brown@tcu.edu>
2013-07-19 12:25:09 -0500

    new

--- lib/devise_sip2_authenticatable.rb
+++ lib/devise_sip2_authenticatable.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 
 require 'devise'
-require 'sip2_authenticatable/adapter'
+require 'devise_sip2_authenticatable/adapter'
 
 Devise.add_module(:sip2_authenticatable, {
   :route => :session,
   :strategy   => true,
   :controller => :sessions,
-  :model  => "sip2_authenticatable/model"
+  :model  => "devise_sip2_authenticatable/model"
 })
